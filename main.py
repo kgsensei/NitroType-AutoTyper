9464d9ce025f68020b8a2c673a957480ecd101fa
Jeremy Kellem <60828719+kgsensei@users.noreply.github.com>
2021-02-25 09:40:56 -0500

    Fixed issue where some characters didn't show up.

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from msedge.selenium_tools import Edge, EdgeOptions
-import time
-import pyautogui as pya
-import keyboard
-webdriver_location = "MicrosoftWebDriver.exe"
-options = EdgeOptions()
-options.use_chromium = True
+import time, pyautogui as pya, keyboard
+webdriver_location="MicrosoftWebDriver.exe"
+options=EdgeOptions()
+options.use_chromium=True
 options.binary_location=r'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe'
-browser = Edge(options=options, executable_path=webdriver_location)
+browser=Edge(options=options,executable_path=webdriver_location)
 browser.get("https://www.nitrotype.com/race")
-running = False
-pya.moveTo(565, 25)
-pya.dragTo(565, 0, 0.3, button='left')
+running=False
 while True:
     if keyboard.is_pressed('/') == True and running == False:
-        runningEFE = True
-        typestring = []
-        print('reading...')
-        elements = browser.find_elements_by_class_name('dash-letter')
+        runningEFE=True
+        typestring=[]
+        print('Reading...')
+        elements=browser.find_elements_by_class_name('dash-letter')
         elements.pop()
         for e in elements:
-            typestring.append(e.text)
+            text=e.get_attribute("textContent")
+            if "\xa0" in text:
+                text=str(text).replace("\xa0"," ")
+            typestring.append(text)
         print(typestring)
         for c in typestring:
             pya.write(c)
-        runningEFE = False
+        runningEFE=False
         print("Ready!")
+
+# Account info
+# ggnore_1
+# igetitimhacking
